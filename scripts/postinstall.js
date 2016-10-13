d809f50144b55bb982a457aa66ec2d7a15d95d91
David Andrus <davidandrus@Davids-MacBook-Pro.local>
2016-10-13 09:52:53 -0700

    adding redux-connect to login and creating base dummy reduxer

--- scripts/postinstall.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs-extra';
-import path from 'path';
-
-const normalizePath = path.join(__dirname, '..', 'node_modules', 'normalize-css', 'normalize.css');
-
-fs.mkdirs('static', (err) => {});
-fs.copy(normalizePath, 'static/normalize.css');
--- src/reducers/counter.js
+++
@@ -1,7 +0,0 @@
-import { handleActions } from 'redux-actions';
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../constants/actionTypes';
-
-export default handleActions({
-  [INCREMENT_COUNTER]: state => state + 1,
-  [DECREMENT_COUNTER]: state => state - 1,
-}, 0);
