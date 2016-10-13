caaa7d65ffb6adcd83ff0a192b4d1adfd93eff2a
David Andrus <davidandrus@Davids-MacBook-Pro.local>
2016-10-13 08:38:08 -0700

    getting all form elements coded

--- src/components/AllViews.jsx
+++ src/components/AllViews.jsx
@@ -3,19 +3,21 @@ import AppBar from 'material-ui/AppBar';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import Register from './Register';
 import Login from './Login';
+import CreateEvent from './CreateEvent';
 
 export default class AllViews extends Component {
   render() {
     return (
-      <div>
+      <div style={{}}>
         <AppBar title="Meet-Up Event Planner" />
         <Tabs>
           <Tab label="Register" />
           <Tab label="Login" />
         </Tabs>
-        <div style={{ maxWidth: '500px', margin: '0 auto' }}>
+        <div style={{ maxWidth: '500px', margin: '0 auto', padding: '0 20px' }}>
           <Register />
           <Login />
+          <CreateEvent />
         </div>
       </div>
     )
