5fd6d0f9960a9610ff7c8915a962f605f0be4bec
David Andrus <davidandrus@Davids-MacBook-Pro.local>
2016-10-23 01:12:45 -0700

    adding redux-react-router to prod store config

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { routerMiddleware } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 import rootReducer from '../reducers';
 
-const enhancer = applyMiddleware(thunk);
+const router = routerMiddleware(browserHistory);
+const enhancer = applyMiddleware(thunk, router);
 
 function configureStore(initialState) {
   return createStore(rootReducer, initialState, enhancer);
 }
-
 
 export default configureStore();
