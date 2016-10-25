57ce89e3ca0a81f813c7cb432614222d8a07ea97
David Andrus <davidandrus@Davids-MacBook-Pro.local>
2016-10-24 23:43:45 -0700

    abstracting pickerbutton logic

---
+++ src/components/PickerButton.jsx
@@ -0,0 +1,33 @@
+import React, { cloneElement, Component, PropTypes } from 'react';
+import IconButton from 'material-ui/IconButton';
+
+export default class PickerButton extends Component {
+  constructor(...args) {
+    super(...args);
+    this._openDialog = this._openDialog.bind(this);
+  }
+
+  _openDialog() {
+    this._dialog.show();
+  }
+
+  render() {
+    const dialog = cloneElement(this.props.dialog, {
+      ref: (element) => { this._dialog = element; },
+    });
+
+    return (
+      <div>
+        <IconButton onClick={this._openDialog}>
+          {this.props.icon}
+        </IconButton>
+        {dialog}
+      </div>
+    );
+  }
+}
+
+PickerButton.propTypes = {
+  icon: PropTypes.node,
+  dialog: PropTypes.node,
+};
--- src/components/TimePickerButton.jsx
+++ src/components/TimePickerButton.jsx
@@ -1,33 +1,15 @@
-import React, { PropTypes, Component } from 'react';
-import IconButton from 'material-ui/IconButton';
+import React, { PropTypes } from 'react';
 import Schedule from 'material-ui/svg-icons/action/schedule';
 import TimePickerDialog from 'material-ui/TimePicker/TimePickerDialog';
+import PickerButton from './PickerButton';
 
-export default class TimePickerButton extends Component {
-  constructor(...args) {
-    super(...args);
-    this._openDialog = this._openDialog.bind(this);
-  }
-
-  _openDialog() {
-    this._dialog.show();
-  }
-
-  render() {
-    const { onSelect } = this.props;
-
-    return (
-      <div>
-        <IconButton onClick={this._openDialog}>
-          <Schedule />
-        </IconButton>
-        <TimePickerDialog
-          onAccept={onSelect}
-          ref={(dialog) => { this._dialog = dialog; }}
-        />
-      </div>
-    );
-  }
+export default function TimePickerButton({ onSelect }) {
+  return (
+    <PickerButton
+      icon={<Schedule />}
+      dialog={<TimePickerDialog onAccept={onSelect} />}
+    />
+  );
 }
 
 TimePickerButton.propTypes = {
