e5498da2b004ca528e6064464fc075cac0c75350
davidandrus <davidandrus.com@gmail.com>
2016-10-29 04:34:57 -0700

    Adding validation for when date and time pickers use input type=text

--- src/utils/date.js
+++ src/utils/date.js
@@ -7,8 +7,8 @@ import {
 } from '../constants/formats';
 
 export function getDateFromValue(value) {
-  const currentLegacyDate = moment(value, legacyDate);
-  const currentModernDate = moment(value, modernDate);
+  const currentLegacyDate = moment(value, legacyDate, true);
+  const currentModernDate = moment(value, modernDate, true);
 
   if (currentLegacyDate.isValid()) { return currentLegacyDate; }
   if (currentModernDate.isValid()) { return currentModernDate; }
@@ -16,8 +16,8 @@ export function getDateFromValue(value) {
 }
 
 export function getTimeFromValue(value) {
-  const currentLegacyTime = moment(value, legacyTime);
-  const currentModernTime = moment(value, modernTime);
+  const currentLegacyTime = moment(value, legacyTime, true);
+  const currentModernTime = moment(value, modernTime, true);
 
   if (currentLegacyTime.isValid()) { return currentLegacyTime; }
   if (currentModernTime.isValid()) { return currentModernTime; }
