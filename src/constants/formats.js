929ec80523fe052c3e4cf357743549e73bc3c06c
David Andrus <davidandrus.com@gmail.com>
2016-11-05 22:27:37 -0700

    fix: change legacy format so time will display in 12 hour instead of 24 hour time

--- src/constants/formats.js
+++ src/constants/formats.js
@@ -1,4 +1,4 @@
 export const modernDate = 'YYYY-MM-DD';
 export const legacyDate = 'MM/DD/YYYY';
 export const modernTime = 'HH:mm';
-export const legacyTime = 'HH:mm A';
+export const legacyTime = 'hh:mm A';
