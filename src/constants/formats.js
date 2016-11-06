5553d0e145a95f0fa60b91e6e7a2ceb0c8471a2d
David Andrus <davidandrus.com@gmail.com>
2016-11-06 01:47:08 -0700

    fix: event list date formatting showing correct time now

--- src/constants/formats.js
+++ src/constants/formats.js
@@ -2,3 +2,4 @@ export const modernDate = 'YYYY-MM-DD';
 export const legacyDate = 'MM/DD/YYYY';
 export const modernTime = 'HH:mm';
 export const legacyTime = 'hh:mm A';
+export const longDateTime = 'MM/DD/YY - hh:mma';
